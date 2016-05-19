919c736dc3691d4bffce03af2442b0eeb9671330
Noelle Knight <noelle.knight@gmail.com>
2016-05-19 16:47:06 -0400

    Fixed form duration bug.

--- client/js/app.js
+++ client/js/app.js
@@ -269,23 +269,32 @@
 
   }
 })();
-;// (function() {
-//   'use strict';
-//
-//   angular
-//   .module('app')
-//   .factory('SequenceService', SequenceService);
-//
-//   SequenceService.$inject =  ['$firebaseArray'];
-//
-//   function SequenceService ($firebaseArray) {
-//       var poses = new Firebase('https://yogibuild.firebaseio.com/sequences');
-//
-//
-//
-//
-//
-//   }
-// }());
+;(function() {
+  'use strict';
+
+  angular
+  .module('app')
+  .factory('SequenceService', SequenceService);
+
+  SequenceService.$inject =  ['$firebaseArray'];
+
+  function SequenceService ($firebaseArray) {
+      var sequences = new Firebase('https://yogibuild.firebaseio.com/sequences');
+      var sequenceList = $firebaseArray(sequences);
+
+      return {
+        createSequence: createSequence,
+        sequenceList: sequenceList
+      };
+
+      function createSequence(newSequence) {
+        if (typeof newSequence === "object"){
+          return $firebaseArray(sequences).$add(newSequence);
+        } else {
+          return null;
+        }
+      }
+  }
+}());
 
 //# sourceMappingURL=app.js.map
--- src/sequences/create-sequences.service.js
+++ src/sequences/create-sequences.service.js
@@ -1,18 +1,27 @@
-// (function() {
-//   'use strict';
-//
-//   angular
-//   .module('app')
-//   .factory('SequenceService', SequenceService);
-//
-//   SequenceService.$inject =  ['$firebaseArray'];
-//
-//   function SequenceService ($firebaseArray) {
-//       var poses = new Firebase('https://yogibuild.firebaseio.com/sequences');
-//
-//
-//
-//
-//
-//   }
-// }());
+(function() {
+  'use strict';
+
+  angular
+  .module('app')
+  .factory('SequenceService', SequenceService);
+
+  SequenceService.$inject =  ['$firebaseArray'];
+
+  function SequenceService ($firebaseArray) {
+      var sequences = new Firebase('https://yogibuild.firebaseio.com/sequences');
+      var sequenceList = $firebaseArray(sequences);
+
+      return {
+        createSequence: createSequence,
+        sequenceList: sequenceList
+      };
+
+      function createSequence(newSequence) {
+        if (typeof newSequence === "object"){
+          return $firebaseArray(sequences).$add(newSequence);
+        } else {
+          return null;
+        }
+      }
+  }
+}());
